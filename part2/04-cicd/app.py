38811f232e33732b7071dc2e5a22280793e7975a
woongjoonchoi <oongjoon10@gmail.com>
2021-12-29 08:15:10 +0000

    sixth commit

--- part2/04-cicd/app.py
+++ part2/04-cicd/app.py
@@ -18,7 +18,7 @@ root_password = 'password'
 
 
 def main():
-    st.title("Mask Classification Model - Wjc Github Actiond  Fifth Deploy Test")
+    st.title("Mask Classification Model - Wjc Github Actiond  Sixth  Deploy Test")
 
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
